8a40973d2315fe4b40b42c82418bdd77c046c975
Leonardo Scicchitano <leo.tomasi@gmail.com>
2014-09-27 19:32:32 -0300

    Fixes y merge fix.

--- src/com/ort/borgplayer/MainActivity.java
+++ src/com/ort/borgplayer/MainActivity.java
@@ -64,8 +64,8 @@ public class MainActivity extends Activity {
 		super.onStart();
 		if(playIntent == null){
 			playIntent = new Intent(this, MusicService.class);
-			bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
 			startService(playIntent);
+			bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
 		}
 	}
 
